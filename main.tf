e61aff4455bacb41f21b602d153c09f7ec1ca971
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2024-12-27 16:56:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,6 @@ resource "azurerm_resource_group" "rg-name" {
   location = var.location             # Reference to the variable for the location
 }
 
-# Fetch the Key Vault based on environment
-#data "azurerm_key_vault" "keyvault" {
-data "azurerm_resource_group" "resourcegroup_name" {
-  name                = data.azurerm_resource_group.rg-name.name  # Provide the resource group name where the Key Vault exists
-}
-
 
 
 
