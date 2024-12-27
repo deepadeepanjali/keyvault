211921b9edf903d80410dec48cdc6104e320f740
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2024-12-27 17:06:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 data "azurerm_resource_group" "resourcegroup_name" {
   location            = var.location #== "prod" ? "shared-prod-vault1" : "shared-non-prod-vault1"
-  resourcegroup  = var.resourcegroup  # Provide the resource group name where the Key Vault exists
+  name  = var.resourcegroup  # Provide the resource group name where the Key Vault exists
 }
 
 
