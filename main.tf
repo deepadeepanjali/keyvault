1a2b27f3c73ae5bbffbf96d025559b98fd6bc581
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2024-12-27 15:48:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,9 @@ provider "azurerm" {
 }
 
 # Fetch the Key Vault based on environment
-data "azurerm_key_vault" "keyvault" {
-  name                = var.kvname #== "prod" ? "shared-prod-vault1" : "shared-non-prod-vault1"
+#data "azurerm_key_vault" "keyvault" {
+data "azurerm_resource_group" "resourcegroup_name" {
+  location            = var.location #== "prod" ? "shared-prod-vault1" : "shared-non-prod-vault1"
   resource_group_name = var.resourcegroup  # Provide the resource group name where the Key Vault exists
 }
 
